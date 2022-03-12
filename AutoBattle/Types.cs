c5a26434d9e790e62a8dd6972942c6a6b712ec2b
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2022-03-11 22:16:32 -0300

    Create IntVector2 + Modify GridBox with IntVector2 + Code Convention
    IntVector2 is a simple representation of a two-dimensional vector of integers
    
    GridBox now uses IntVector2

--- AutoBattle/Types.cs
+++ AutoBattle/Types.cs
@@ -16,19 +16,22 @@ namespace AutoBattle
 
         }
 
+        /// <summary>
+        /// Cell that constitutes the battlefield
+        /// </summary>
         public struct GridBox
         {
-            public int xIndex;
-            public int yIndex;
+            public IntVector2 position;
             public bool ocupied;
-            public int Index;
+            public int index;
+            public Character character;
 
-            public GridBox(int x, int y, bool ocupied, int index)
+            public GridBox(int _xIndex, int _yIndex, bool _ocupied, int _index, Character _character = null)
             {
-                xIndex = x;
-                yIndex = y;
-                this.ocupied = ocupied;
-                this.Index = index;
+                position = new IntVector2(_xIndex, _yIndex);
+                ocupied = _ocupied;
+                index = _index;
+                character = _character;
             }
 
         }
@@ -46,6 +49,38 @@ namespace AutoBattle
             Warrior = 2,
             Cleric = 3,
             Archer = 4
+        }
+
+        /// <summary>
+        /// Simple representation of a two-dimensional vector of integers
+        /// </summary>
+        public struct IntVector2
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public IntVector2(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+
+            public void GetDirection(IntVector2 from, IntVector2 to)
+            {
+                if (from.x > to.x)
+                    this.x = -1;
+                else if (from.x < to.x)
+                    this.x = 1;
+                else
+                    this.x = 0;
+
+                if (from.y > to.y)
+                    this.y = -1;
+                else if (from.y < to.y)
+                    this.y = 1;
+                else
+                    this.y = 0;
+            }
         }
 
     }
